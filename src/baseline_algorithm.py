ae0c079a4f92079ff83b0f9e506103260fb186f6
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2024-01-21 22:05:42 +0100

    Baseline algorithm setup

---
+++ src/baseline_algorithm.py
@@ -0,0 +1,26 @@
+"""
+Baseline algorithm for reference
+
+
+Move the cart in the direction of the pendulum angle
+Goal is to keep the pendulum upright (theta = 0)
+"""
+
+import numpy as np
+from src.inverted_pendulum_environment import InvertedPendulumEnvironment
+from src.inverted_pendulum_simulator.src.inverted_pendulum import InvertedPendulum
+from src.inverted_pendulum_simulator.src.inverted_pendulum_visualizer import (
+    InvertedPendulumVisualizer,
+)
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import time
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
