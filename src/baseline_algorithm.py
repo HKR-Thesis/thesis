8d4496a90ea44d4ef9271a561df77a46b4fdd1a1
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2024-01-25 17:31:53 +0100

    Create baseline algorithm for the pendulum

--- src/baseline_algorithm.py
+++ src/baseline_algorithm.py
@@ -6,23 +6,44 @@ Move the cart in the direction of the pendulum angle
 Goal is to keep the pendulum upright (theta = 0)
 """
 
-import numpy as np
-from src.inverted_pendulum_environment import InvertedPendulumEnvironment
-from src.inverted_pendulum_simulator.src.inverted_pendulum import InvertedPendulum
-from src.inverted_pendulum_simulator.src.inverted_pendulum_visualizer import (
+from .inverted_pendulum_simulator.src.inverted_pendulum import InvertedPendulum
+from .inverted_pendulum_simulator.src.inverted_pendulum_visualizer import (
     InvertedPendulumVisualizer,
 )
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
-import time
+
+import matplotlib as plt
 
 
-def main():
-    while True:
-        pendulum_simulation = InvertedPendulum()
-    pass
+def angle_based() -> None:
+    pendulum_simulation = InvertedPendulum()
+    voltage = 60
+
+    try:
+        num_frames = 144 * 100
+        states = []
+
+        for _ in range(num_frames):
+            angle = pendulum_simulation.state[0]
+
+            if angle > 3.125:
+                voltage_command = voltage
+            elif angle < 3.125:
+                voltage_command = -voltage
+            else:
+                voltage_command = 0
+
+            pendulum_simulation.simulate_step(voltage_command)
+            states.append(pendulum_simulation.state.copy())
+            print(pendulum_simulation.state)
+
+        pendulum_simulation.simulate_step(0)
+        states.append(pendulum_simulation.state.copy())
+        visualizer = InvertedPendulumVisualizer(pendulum_simulation)
+        visualizer.animate_states(states)
+
+    except KeyboardInterrupt:
+        plt.close()
 
 
 if __name__ == "__main__":
-    main()
+    angle_based()
